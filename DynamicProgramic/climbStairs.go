a1a87a300cc187217dbd3df42033ea058d20fc63
changjunhang <changjh@mail.tanjie.wang>
2020-08-22 18:00:49 +0800

    爬楼梯

---
+++ DynamicProgramic/climbStairs.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+/*
+	dp[n] 表示能到达第 n 阶的方法总数
+*/
+
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	dp := make([]int, n+1)
+	/*
+		dp[0] = 0，根据转移方程，dp[2]=1，但是dp[2]=2，初始值不对。所以dp[2]也是一个初始值
+	*/
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
+
+func main() {
+	fmt.Println(climbStairs(10))
+}
