182c6e4e464e70966d9b343ed537001d3dd94a21
changjunhang <changjh@mail.tanjie.wang>
2020-08-31 18:19:40 +0800

    完全背包问题

--- DynamicProgramic/beibao/wanquan.go
+++ DynamicProgramic/beibao/wanquan.go
@@ -1,1 +1,34 @@
 package main
+
+import "fmt"
+
+const Nw = 1010
+
+func wanquan() int {
+	var a, b int
+	fmt.Scanf("%d %d\n", &a, &b)
+	v1 := make([]int, Nw)
+	w1 := make([]int, Nw)
+	f1 := make([]int, Nw)
+	for i := 1; i <= a; i++ {
+		fmt.Scanf("%d %d\n", &v1[i], &w1[i])
+	}
+	for i := 1; i <= a; i++ {
+		for j := v1[i]; j <= b; j++ {
+			f1[j] = max2(f1[j], f1[j-v1[i]]+w1[i])
+		}
+	}
+	return f1[b]
+}
+
+func max2(a, b int) int {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func main() {
+	fmt.Println(wanquan())
+}
