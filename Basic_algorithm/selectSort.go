665a357d107d3316cfb2db7286c59e127f25b0e6
changjunhang <l>
2020-10-20 17:05:48 +0800

    选择排序

--- Basic_algorithm/selectSort.go
+++ Basic_algorithm/selectSort.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func selectSort(arr []int) []int {
+	length := len(arr)
+	for i := 0; i < length-1; i++ {
+		min := i
+		for j := i + 1; j < length; j++ {
+			if arr[min] > arr[j] {
+				min = j
+			}
+		}
+		arr[i], arr[min] = arr[min], arr[i]
+	}
+	return arr
+}
+
+func main() {
+	list := []int{3, 2, 5, 7, 9}
+	fmt.Println(selectSort(list))
+}
